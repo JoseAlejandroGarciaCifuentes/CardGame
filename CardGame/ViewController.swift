8772ac153f95b405e308aaefab13c9abaaa3f4ea
Alejandro <alex.gar.cifu@gmail.com>
2020-11-16 19:47:16 +0100

    small changes, reset game feature

--- CardGame/ViewController.swift
+++ CardGame/ViewController.swift
@@ -10,50 +10,70 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    
     @IBOutlet weak var rightNumber: UIButton!
     @IBOutlet weak var leftnumber: UIButton!
     @IBOutlet weak var currentScore: UIButton!
-    //@IBOutlet weak var currentScore: UILabel!
-    var currentNumber: Int = 0;
+
+    var currentNumber: Int = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        currentScore.setTitle(Int.random(in: 0..<21).description, for: .normal);
-        leftnumber.setTitle(getRandomNumber().description, for: .normal);
-        rightNumber.setTitle(getRandomNumber().description, for: .normal);
-        // Do any additional setup after loading the view.
+        
+        setCardValues()
+       
     }
-
-    
-        //currentScore.text! = getRandomNumber().description;
     
     
     @IBAction func leftButtonAction(_ sender: Any) {
         
+        let cardValue: Int = Int (leftnumber.title(for: .normal)!.description)!
         
-        let currentScoreInt: Int = Int (currentScore.title(for: .normal)!.description)!;
-        let leftNumberInt: Int = Int (leftnumber.title(for: .normal)!.description)!;
+        let score = updateScore(cardValue: cardValue)
         
-        let result: Int = currentScoreInt + leftNumberInt;
+        leftnumber.setTitle(getRandomNumber().description, for: .normal)
         
-        currentScore.setTitle(result.description, for: .normal);
-        leftnumber.setTitle(getRandomNumber().description, for: .normal);
+        checkState(score: score)
         
     }
     @IBAction func rightButtonAction(_ sender: UIButton) {
         
-        let currentScoreInt: Int = Int (currentScore.title(for: .normal)!.description)!;
-        let rightNumberInt: Int = Int (rightNumber.title(for: .normal)!.description)!;
+        let cardValue: Int = Int (rightNumber.title(for: .normal)!.description)!
         
-        let result: Int = currentScoreInt + rightNumberInt;
+        let score = updateScore(cardValue: cardValue)
         
-        currentScore.setTitle(result.description, for: .normal);
-        rightNumber.setTitle(getRandomNumber().description, for: .normal);
+        rightNumber.setTitle(getRandomNumber().description, for: .normal)
+        
+        checkState(score: score)
     }
     
     func getRandomNumber() -> Int {
         return Int.random(in: -9..<9)
     }
+    
+    func setCardValues(){
+        currentScore.setTitle(Int.random(in: 0..<21).description, for: .normal)
+        leftnumber.setTitle(getRandomNumber().description, for: .normal)
+        rightNumber.setTitle(getRandomNumber().description, for: .normal)
+    }
+    
+    func updateScore(cardValue: Int)->Int{
+        
+        let currentScoreInt: Int = Int (currentScore.title(for: .normal)!.description)!
+        let result: Int = currentScoreInt + cardValue
+        currentScore.setTitle(result.description, for: .normal)
+        
+        return result
+    }
+    
+    func checkState(score:Int) {
+        
+        if score > 21 || score < 0 {
+            performSegue(withIdentifier: "lostGame", sender: nil)
+        }else{
+            //SUMAR PUNTOS
+        }
+    }
+
+    
 }
 
